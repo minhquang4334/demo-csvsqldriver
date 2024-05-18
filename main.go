162c2512aba2e5b96a40d9cfe03dac6e47e27cdd
minhquang4334 <minhquang4334@gmail.com>
2024-05-19 00:07:40 +0700

    refactor

--- main.go
+++ main.go
@@ -15,33 +15,25 @@ func main() {
 	}
 
 	defer db.Close()
-	if err := selectAll(db); err != nil {
-		log.Fatalf("Error %s when selecting\n", err)
-	}
-}
-
-func selectAll(db *sql.DB) error {
 	rows, err := db.Query("SELECT * FROM csv")
 	if err != nil {
-		return err
+		log.Fatalf("Error %s when querying DB\n", err)
 	}
 
 	defer rows.Close()
 
 	columns, err := rows.Columns()
 	if err != nil {
-		return err
+		log.Fatalf("Error %s when getting columns\n", err)
 	}
 
 	for rows.Next() {
 		var f1, f2, f3 string
 		err := rows.Scan(&f1, &f2, &f3)
 		if err != nil {
-			return err
+			log.Fatalf("Error %s when scanning\n", err)
 		}
 
 		fmt.Printf("%s=%s, %s=%s, %s=%s\n", columns[0], f1, columns[1], f2, columns[2], f3)
 	}
-
-	return nil
 }
